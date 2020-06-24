f74919bb5f98cc6a880aa00fbdd8b588fcc29886
Jackson Cai <jcai8649@gmail.com>
2020-06-24 14:50:07 -0700

    Updates

---
+++ precache-manifest.c30b203c7045bbf7de146874320e11fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1486d987f26cbc9f34bb5366fd651d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "f35b40c407405447dabd",
+    "url": "/pokedex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "36848cff275c6d7d3d0e",
+    "url": "/pokedex/static/css/main.f45ba1e0.chunk.css"
+  },
+  {
+    "revision": "f35b40c407405447dabd",
+    "url": "/pokedex/static/js/2.44990259.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.44990259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36848cff275c6d7d3d0e",
+    "url": "/pokedex/static/js/main.2f1c87ab.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "510f8c1e1db5baf7478da8dbcf1044e3",
+    "url": "/pokedex/static/media/Pokemon Solid.510f8c1e.woff"
+  },
+  {
+    "revision": "9c662459ee4705ed911e61be7509d022",
+    "url": "/pokedex/static/media/pokemon_fire_red.9c662459.woff"
+  }
+]);
